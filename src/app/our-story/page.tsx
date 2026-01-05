62a4fa83a9aeb0d421e2f5f191f4e0dce360e8cc
Neo Mokhele <neo.mokhele@bitcube.tech>
2026-01-05 21:16:50 +0200

    feat(our-story): added chef image responsive with rotation

--- src/app/our-story/page.tsx
+++ src/app/our-story/page.tsx
@@ -3,6 +3,7 @@ import { PaddingContainer } from "@/components/structure/PaddingContainer";
 import { PageContainer } from "@/components/structure/PageContainer";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export default function OurStoryPage() {
@@ -18,9 +19,9 @@ export default function OurStoryPage() {
         />
       </div>
 
-      <section className="flex flex-col w-full lg:p-12 py-12 px-8">
+      <section className="flex flex-col w-full lg:px-12 lg:py-12 py-12 px-4">
         <PaddingContainer size="large">
-          <div className="flex lg:flex-row flex-col gap-6">
+          <div className="flex lg:flex-row flex-col gap-12 items-center">
             <div className="flex flex-col gap-6 lg:w-2/3 w-full">
               <p className="flex items-center gap-4 font-normal text-xxs text-crimson-600 uppercase">
                 <span className="w-16 border-t border-crimson-600"></span> the
@@ -28,23 +29,23 @@ export default function OurStoryPage() {
               </p>
 
               <div className="flex flex-col w-full">
-                <h1 className="font-sans !font-extrabold lg:text-6xl text-4xl text-white">
+                <h1 className="font-sans !font-extrabold lg:text-6xl text-3xl text-white">
                   Rooted in tradition,
                   <br />
                 </h1>
-                <h2 className="font-normal lg:text-6xl text-4xl text-white-60 italic lg:-mt-4 -mt-1">
+                <h2 className="font-normal lg:text-6xl text-3xl text-white-60 italic lg:-mt-4 -mt-1">
                   Daring in Execution
                 </h2>
               </div>
 
               <div className="flex flex-col gap-4">
-                <p className="font-normal text-sm text-white">
+                <p className="font-normal lg:text-sm text-xs text-white">
                   We believe in the purity of flaws. Our kitchen merges ancient
                   culinary techniques with modern flair to create a dining
                   experience that transcends the ordinary.
                 </p>
 
-                <p className="font-normal text-sm text-white">
+                <p className="font-normal lg:text-sm text-xs text-white">
                   Every plate is a canvas, and every ingredient is a color. We
                   strive not just to feed the belly, but to spark a memory, to
                   evolve in a world of culinary possibilities and artistry,
@@ -61,7 +62,18 @@ export default function OurStoryPage() {
               </Button>
             </div>
 
-            <div className="lg:w-1/2 w-full"></div>
+            <div className="lg:w-1/2 w-full flex justify-center">
+              <div className="relative w-full max-w-sm sm:max-w-md lg:max-w-lg aspect-[4/5] rounded-2xl bg-burgundy-900 p-3 rotate-2 flex items-center justify-center">
+                <Image
+                  src="/chef-antonie.jpg"
+                  alt="chef-antonie"
+                  fill
+                  priority
+                  sizes="(max-width: 640px) 90vw, (max-width: 1024px) 60vw, 500px"
+                  className="object-cover rounded-2xl -rotate-6"
+                />
+              </div>
+            </div>
           </div>
         </PaddingContainer>
       </section>
