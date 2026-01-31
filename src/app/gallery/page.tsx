ad83cc30400f6419d7f7996f6c3219ac57d7fc90
Neo Mokhele <neo.mokhele@bitcube.tech>
2026-01-31 21:19:00 +0200

    fear(coming-soon): updated UI and image toggle

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import { ComingSoonComponent } from "@/components/coming-soon-component";
 
 export default function GalleryPage() {
-  return <div></div>;
+  return <ComingSoonComponent />;
 }
