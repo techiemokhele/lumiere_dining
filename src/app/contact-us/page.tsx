04ab829584e318df3550cccacf0a2cfa88eea41d
Neo Mokhele <neo.mokhele@bitcube.tech>
2026-02-08 17:09:04 +0200

    feat(contact-us): add google maps for company address

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -140,7 +140,7 @@ export default function ContactPage() {
         />
       </div>
 
-      <PaddingContainer size="small">
+      <PaddingContainer size="small" className="flex flex-col gap-20">
         <div className="flex lg:flex-row flex-col w-full gap-10">
           <div className="flex flex-col gap-8 lg:w-1/2 w-full items-start">
             <div className="flex flex-row items-center gap-4 w-full">
@@ -388,6 +388,20 @@ export default function ContactPage() {
               </form>
             </Form>
           </div>
+        </div>
+
+        <div className="w-full h-[600px] rounded-2xl overflow-hidden border-2 border-burgundy-700 shadow-lg">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.4839449620636!2d18.41893!3d-33.91483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc676c0b0b0b0b%3A0x0!2s19%20Dock%20Rd%2C%20V%26A%20Waterfront%2C%20Cape%20Town%2C%208001!5e0!3m2!1sen!2sza!4v1234567890123!5m2!1sen!2sza"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Lumiere Dining Location - 19 Dock Road, Cape Town"
+            className="w-full h-full"
+          />
         </div>
       </PaddingContainer>
     </PageContainer>
